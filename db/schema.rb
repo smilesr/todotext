a09850006d8132e99bcf933ccfaa1e904e7afe44
Robert Smiles Jr <smilesr120@gmail.com>
2015-10-18 15:43:20 -0400

    migrated 2 columns to tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151018173738) do
+ActiveRecord::Schema.define(version: 20151018192259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(version: 20151018173738) do
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.datetime "due_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "completion", default: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
